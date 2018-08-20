229207d4bfe8a730d0b6ecb64c1d8195e0361d98
nadav <nadavmailspam@gmail.com>
2018-08-20 13:03:20 +0300

    assignment ready! :]]

--- vagrant/fixScripts/exercise2-fix.sh
+++ vagrant/fixScripts/exercise2-fix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 #add fix to exercise2 here
+sed -i 's/www.ascii-art.de/server1/g' /etc/hosts
--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen  -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ""
+chown vagrant /home/vagrant/.ssh/id_rsa /home/vagrant/.ssh/id_rsa.pub
+sed -i 's/#   StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
